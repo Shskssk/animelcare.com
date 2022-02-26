4f5b3a1107e0087a13f1875a4a56871442bc5a1a
Shskssk <99404488+Shskssk@users.noreply.github.com>
2022-02-26 18:43:59 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function mydocument () {
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+}
+
+
+function scroolInDiv(){
+    var elmntToView = document.getElementById("down-a");
+        elmntToView.scrollIntoView();   
+}
+
+
+function scroolInDiv1(){
+    var elmntToView = document.getElementById("down-b");
+        elmntToView.scrollIntoView();   
+}
+
+function scroolInDiv2(){
+    var elmntToView = document.getElementById("down-c");
+        elmntToView.scrollIntoView();   
+}
+
+function scroolInDiv5(){
+    var elmntToView = document.getElementById("down-d");
+        elmntToView.scrollIntoView();   
+}
